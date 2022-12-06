cfa2471dc877289b3d79c45c7ee5b6c14bc9fbdb
phuocanhtts <109740636+phuocanhtts@users.noreply.github.com>
2022-12-06 23:02:35 +0700

    phuocanhup061222

--- src/components/detialproduct/DetailProduct.jsx
+++ src/components/detialproduct/DetailProduct.jsx
@@ -1,30 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./DetailProduct.scss";
 
 const DetailProduct = (props) => {
   const [stateDetailProduct, setStateDetailProduct] = useState(props.product);
-  console.log("props detail", props);
+  console.log("props", props);
 
   return (
     <>
       <div class="wrapper">
-        <div className="d-flex">
-          <div class="product-img col-6">
-            <img class="content-img" src={stateDetailProduct.img} />
+        <div class="product-img">
+          <img class="content-img" src={stateDetailProduct.img} />
+        </div>
+        <div class="product-info">
+          <div class="product-text">
+            <h1>{stateDetailProduct.name}</h1>
+            <p>
+              Harvest Vases are a reinterpretation of peeled fruits and
+              vegetables as functional objects.<br></br> The surfaces appear to
+              be sliced and pulled aside,allowing room for growth.
+            </p>
           </div>
-          <div class="product-info col-6">
-            <div class="product-text">
-              <h1>{stateDetailProduct.name}</h1>
-              <p>
-                Harvest Vases are a reinterpretation of peeled fruits and
-                vegetables as functional objects. The surfaces appear to be
-                sliced and pulled aside,allowing room for growth.
-              </p>
-            </div>
-            <div class="product-price-btn">
-              <p>
-                <span className="span-detail">{stateDetailProduct.price}</span>$
-              </p>
+          <div class="product-price-btn">
+            <div className="price-detail">{stateDetailProduct.price}</div>
+            <div className="bt">
               <button class="button-buy" type="button">
                 Buy now
               </button>
