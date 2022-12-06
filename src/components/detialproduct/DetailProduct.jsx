23bed19ff9085d1a3efc39d415fbca173c3165b1
phuocanhtts <109740636+phuocanhtts@users.noreply.github.com>
2022-12-06 21:55:51 +0700

    pa061222 up

--- src/components/detialproduct/DetailProduct.jsx
+++ src/components/detialproduct/DetailProduct.jsx
@@ -8,25 +8,23 @@ const DetailProduct = (props) => {
   return (
     <>
       <div class="wrapper">
-        <div className="d-flex">
-          <div class="product-img col-6">
-            <img class="content-img" src={stateDetailProduct.img} />
+        <div class="product-img">
+          <img class="content-img" src={stateDetailProduct.img} />
+        </div>
+        <div class="product-info">
+          <div class="product-text">
+            <h1>{stateDetailProduct.name}</h1>
+            <p>
+              Harvest Vases are a reinterpretation of peeled fruits and
+              vegetables as functional objects.<br></br> The surfaces appear to
+              be sliced and pulled aside,allowing room for growth.
+            </p>
           </div>
-          <div class="product-info col-6">
-            <div class="product-text">
-              <h1>{stateDetailProduct.name}</h1>
-              <p>
-                Harvest Vases are a reinterpretation of peeled fruits and
-                vegetables as functional objects. The surfaces appear to be
-                sliced and pulled aside,allowing room for growth.{" "}
-              </p>
-            </div>
-            <div class="product-price-btn">
-              <p>
-                <span className="span-detail">{stateDetailProduct.price}</span>$
-              </p>
+          <div class="product-price-btn">
+            <div className="price-detail">{stateDetailProduct.price}</div>
+            <div className="bt">
               <button class="button-buy" type="button">
-                buy now
+                Buy now
               </button>
             </div>
           </div>
